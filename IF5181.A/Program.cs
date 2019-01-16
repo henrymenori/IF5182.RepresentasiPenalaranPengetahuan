cc6d7940835ca444bb5b9ec99e0f62a7d0fab3f7
menori <henrymenori@yahoo.com>
2019-01-16 21:40:03 +0700

    Add tree representation

---
+++ IF5181.A/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF5181.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = null;
+            Console.Write("Input  : ");
+            var command = Console.ReadLine().Split(' ');
+
+            while (command != null && command.Length > 0 && command[0] != "exit")
+            {
+                Console.Write("Output : ");
+
+                if (command[0] == "add")
+                {
+                    if (tree == null)
+                    {
+                        tree = new Tree(command[1]);
+                        Console.WriteLine(string.Format("Word '{0}' added successfully", command[1]));
+                    }
+                    else
+                    {
+                        if (tree.Add(command[1]))
+                            Console.WriteLine(string.Format("Word '{0}' added successfully", command[1]));
+                        else
+                            Console.WriteLine(string.Format("Word '{0}' already exist", command[1]));
+                    }
+                }
+                else if (command[0] == "search")
+                {
+                    if (tree == null)
+                    {
+                        Console.WriteLine(string.Format("Word '{0}' is not found", command[1]));
+                    }
+                    else
+                    {
+                        if (tree.Search(command[1]))
+                            Console.WriteLine(string.Format("Word '{0}' found", command[1]));
+                        else
+                            Console.WriteLine(string.Format("Word '{0}' is not found", command[1]));
+                    }
+                }
+                else if (command[0] == "print")
+                {
+                    if (tree == null)
+                    {
+                        Console.WriteLine("Empty Tree");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(tree.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Command not recognized");
+                }
+
+                Console.WriteLine();
+                Console.Write("Input  : ");
+                command = Console.ReadLine().Split(' ');
+            }
+        }
+    }
+}
---
+++ IF5181.A/Tree.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF5181.A
+{
+    class Tree
+    {
+        #region Properties
+
+        public bool Stop { get; set; }
+        public char Data { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Tree(char c)
+        {
+            Data = c;
+            Right = null;
+            Left = null;
+            Stop = false;
+        }
+
+        public Tree(string s)
+        {
+            Data = s.First();
+            Right = null;
+            Left = s.Length > 1 ? new Tree(s.Substring(1)) : null;
+            Stop = s.Length <= 1;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public bool Add(string word)
+        {
+            var valid = true;
+
+            if (word.First() < Data)
+            {
+                var stopTemp = Stop;
+                var dataTemp = Data;
+                var leftTemp = Left;
+                var rightTemp = Right;
+
+                Data = word.First();
+                Stop = word.Length <= 1;
+                Left = word.Length > 1 ? new Tree(word.Substring(1)) : null;
+                Right = new Tree(dataTemp)
+                {
+                    Stop = stopTemp,
+                    Left = leftTemp,
+                    Right = rightTemp
+                };
+            }
+            else if (word.First() > Data)
+            {
+                if (Right == null)
+                    Right = new Tree(word);
+                else
+                    valid = Right.Add(word);
+            }
+            else
+            {
+                if (word.Length > 1)
+                {
+                    if (Left == null)
+                        Left = new Tree(word.Substring(1));
+                    else
+                        valid = Left.Add(word.Substring(1));
+                }
+                else
+                {
+                    if (Stop)
+                        valid = false;
+                    else
+                        Stop = true;
+                }
+            }
+
+            return valid;
+        }
+
+        public bool Search(string word)
+        {
+            var valid = false;
+
+            if (word.First() < Data)
+            {
+
+            }
+            else if (word.First() > Data)
+            {
+                if (Right != null)
+                    valid = Right.Search(word);
+            }
+            else
+            {
+                if (word.Length > 1)
+                {
+                    if (Left != null)
+                        valid = Left.Search(word.Substring(1));
+                }
+                else
+                {
+                    valid = Stop;
+                }
+            }
+
+            return valid;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, ToListString());
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private List<string> ToListString()
+        {
+            var list = new List<string> { string.Format("{0}{1}", Data, Stop ? '*' : ' ') };
+
+            if (Left != null)
+            {
+                var leftList = Left.ToListString();
+                list[0] = string.Format("{0} - {1}", list[0], leftList[0]);
+
+                for (int i = 1; i < leftList.Count; i++)
+                {
+                    list.Add(string.Format("     {0}", leftList[i]));
+                }
+            }
+
+            if (Right != null)
+            {
+                for (int i = 1; i < list.Count; i++)
+                {
+                    list[i] = string.Format("|{0}", list[i].Substring(1));
+                }
+
+                list.Add("|");
+                list.AddRange(Right.ToListString());
+            }
+
+            return list;
+        }
+
+        #endregion
+    }
+}
